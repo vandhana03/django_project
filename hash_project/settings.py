373ee11ec0ca1e9e442bf8754a6a58ad9bb40c0e
Vandhana Mulguri <vandhanamulguri@gmail.com>
2026-04-07 18:56:32 +0530

    allowed_host added

--- hash_project/settings.py
+++ hash_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-project-k01t.onrender.com','localhost:8000']
 
 
 # Application definition
